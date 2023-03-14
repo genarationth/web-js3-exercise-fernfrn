bb2b7135e9f624f6de9020c57f36f493180876c7
Fern Piya S <125846412+fernfrn@users.noreply.github.com>
2023-03-14 14:36:35 +0700

    Create Exercise2.js
    submit homework

---
+++ Exercise2.js
@@ -0,0 +1,23 @@
+// Ex 02
+// part 1
+
+let name1 = 'John';  
+let name2 = 'Smith';
+let name3 = 'Sandy';
+
+const greetingFriends = () => {
+    return `Welcome ${name1}, ${name2}, ${name3}`
+}
+
+console.log(greetingFriends());
+
+
+// part 2
+
+ageNow = (birthYear) => {
+    let year = 2023;
+    ageNow = year - birthYear;
+    return `${birthYear} returns ${ageNow}`;
+}
+
+console.log(ageNow(1991));
