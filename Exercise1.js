397efb094a3b8a3682c8eed590cf69fa82571741
Fern Piya S <125846412+fernfrn@users.noreply.github.com>
2023-03-14 17:26:14 +0700

    Update Exercise1.js
    update file

--- Exercise1.js
+++ Exercise1.js
@@ -1,11 +1,11 @@
 // Ex 01
 // transaction
 
-// transaction fee 3% and interest fee 0.01%
+// transaction fee 3$ and interest fee 0.01%
 
 const transaction = cost => {
-    cost = cost + (cost * 0.03) + (cost * 0.001)
+    cost = cost + (cost + 3) + (cost * 0.001)
     return cost;
 }
 
-console.log(transaction(300));
+console.log(transaction(300)); //603.3
